abfabddc940e95a2108ad4d576a77da8bd13babe
aCube <player559001@gmail.com>
2023-04-26 20:17:09 -0300

    Fix plugin.

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,23 +1,27 @@
 return {
-  -- Misc
-  { "ntpeters/vim-better-whitespace" }, -- Trailing spaces
-  { "davisdude/vim-love-docs" },
-  {
-    "folke/todo-comments.nvim",
-    dependencies = "nvim-lua/plenary.nvim",
-  },
-  {
-    "tzachar/cmp-tabnine",
-    build = "./install.sh",
-    dependencies = "hrsh7th/nvim-cmp",
-  },
-  {
-    "cappyzawa/trim.nvim",
-    event = "BufWritePre",
-    opts = {},
-  },
-  {
-    "p00f/clangd_extensions.nvim",
-    dependencies = "mason-lspconfig.nvim", -- make sure to load after mason-lspconfig
-  },
+	-- Misc
+	{
+		"folke/todo-comments.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+	},
+	{
+		"tzachar/cmp-tabnine",
+		build = "./install.sh",
+		dependencies = "hrsh7th/nvim-cmp",
+	},
+	{
+		"cappyzawa/trim.nvim",
+		event = "BufWritePre",
+		opts = {},
+	},
+	{
+		"p00f/clangd_extensions.nvim",
+		dependencies = "mason-lspconfig.nvim", -- make sure to load after mason-lspconfig
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "clangd" }, -- automatically install lsp
+		},
+	},
 }
